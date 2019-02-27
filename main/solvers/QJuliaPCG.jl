84f4e2e79ff677563eccfc44e35e62072b992ff3
alexstrel <astrel@fnal.gov>
2019-02-27 08:43:20 -0600

    minor changes in PCG solver

--- main/solvers/QJuliaPCG.jl
+++ main/solvers/QJuliaPCG.jl
@@ -39,14 +39,10 @@ function solver(x::AbstractArray, b::AbstractArray, Mat::Any, MatSloppy::Any, pa
 
     println("Running ", solver_name ," solver.")
 
-    if is_preconditioned == true
-      println("Preconditioner  :: ", param.inv_type_precondition)
-    end
+    if is_preconditioned == true; println("Preconditioner  :: ", param.inv_type_precondition); end
 
     if (param.maxiter == 0)
-      if param.use_init_guess == false
-        x .=@. 0.0
-      end
+      if param.use_init_guess == false; x .=@. 0.0; end
       return
     end #if param.maxiter == 0
 
@@ -56,7 +52,7 @@ function solver(x::AbstractArray, b::AbstractArray, Mat::Any, MatSloppy::Any, pa
 
     local r    = Vector{param.dtype}(undef, length(x))
     local y    = Vector{param.dtype}(undef, length(x))
-	# aux high precision vector
+    # aux high precision vector
     local yaux = ones(param.dtype, length(y))
     local yaux2 = zeros(param.dtype, length(y))
     # sloppy residual vector
@@ -67,70 +63,71 @@ function solver(x::AbstractArray, b::AbstractArray, Mat::Any, MatSloppy::Any, pa
     local s       = zeros(param.dtype_sloppy, length(rSloppy))
     # iterated sloppy solution vector
     local xSloppy = param.use_sloppy_partial_accumulator == true ? x : zeros(param.dtype_sloppy, length(rSloppy))
-	# for the preconditioner
+    # for the preconditioner
     local u       = is_preconditioned == true ? zeros(param.dtype_sloppy, length(rSloppy)) : rSloppy
     local rPre    = param.dtype_precondition != param.dtype_sloppy ? zeros(param.dtype_precondition, length(rSloppy)) : rSloppy
     local pPre    = param.dtype_precondition != param.dtype_sloppy ? zeros(param.dtype_precondition, length(rSloppy)) : u
 
-	local plot_data = Array{Float64, 2}(undef, param.maxiter, 4)
+    local plot_data = Array{Float64, 2}(undef, param.maxiter, 4)
 
-	Precond(out, inp) = MatPrecon(out, inp, pPre, rPre,K)
+    Precond(out, inp) = MatPrecon(out, inp, pPre, rPre,K)
 
     b2 = norm2(b)  #Save norm of b
     r2 = 0.0; r2_old = 0.0     #if zero source then we will exit immediately doing no work
     #
-	ϵ     = eps(param.dtype_sloppy) / 2.0; ϵh = eps(param.dtype) / 2.0
-	deps  = sqrt(ϵ); dfac = 1.1; nfact = 10.0*sqrt(Float64(length(r)))
+    ϵ     = eps(param.dtype_sloppy) / 2.0; ϵh = eps(param.dtype) / 2.0
+    deps  = sqrt(ϵ); dfac = 1.1; nfact = 10.0*sqrt(Float64(length(r)))
     xnorm = 0.0; ppnorm = 0.0; Anorm = 0.0
 	#
-	y .=@. 1.0
-	Mat(y, yaux)
-	yaux .=@. abs.(y)
-	Anorm = findmax(yaux)[1]
-	# Relupdates parameters:
+    y .=@. 1.0
+    Mat(y, yaux)
+    yaux .=@. abs.(y)
+    Anorm = findmax(yaux)[1]
+    # Relupdates parameters:
     println("Estimated matrix norm is ", Anorm)
 
     if param.use_init_guess == true
       #r = b - Ax0 <- real
       Mat(r, x)
       r .=@. b - r
-	  r2 = norm2(r)
+      r2 = norm2(r)
       y .=@. x
     else
       r2 = b2;
       r .=@. b
       y .=@. 0.0
     end
-	# Relupdates parameters:
+	#
+    xSloppy .=@. 0.0
+    # Relupdates parameters:
     rUpdate = 0
     rNorm   = sqrt(r2)
     #dinit   = ϵh*(rNorm+nfact*Anorm*xnorm)
-	dinit   = ϵh*(rNorm+(nfact+1)*Anorm*xnorm)
+    dinit   = ϵh*(rNorm+(nfact+1)*Anorm*xnorm)
     dk      = dinit
     #
-	cpy(rSloppy, r)
+    cpy(rSloppy, r)
     #
-	Precond(u, rSloppy)
-    #
-    p  .=@. u; xSloppy .=@. 0.0
+    Precond(u, rSloppy)
     # initialize CG parameters
-    α = 0.0; β = 0.0; pAp = 0.0
-	#
-	γ    = is_preconditioned == true ? rdot(rSloppy, u) : r2
-	γold = 0
+    β = 0.0; γold = 0
+    γ = is_preconditioned == true ? rdot(rSloppy, u) : r2
     #iteration counters
     k = 0; converged = false
 
     # if invalid residual then convergence is set by iteration count only
     stop = b2*param.tol*param.tol
     println(solver_name," : Initial residual = ", sqrt(r2))
+    println(solver_name," : Initial preconditioned residual ", norm(u))
 
     resIncrease = 0; resIncreaseTotal = 0;  relUpdates = 0
 
     updateR::Bool = false
 
     # Main loop:
-    while (k < param.maxiter && converged == false)
+    max_res_incr = 32
+    aux_rel_trigger = max_res_incr
+    while (k < param.maxiter && converged == false && aux_rel_trigger > 0 )
       # Update search vector
       p  .=@. u + β*p
       #
@@ -142,31 +139,34 @@ function solver(x::AbstractArray, b::AbstractArray, Mat::Any, MatSloppy::Any, pa
       #
       α = γ / pAp
       #
-	  xSloppy    .=@. xSloppy + α*p
+      xSloppy    .=@. xSloppy + α*p
 	  #
       rSloppyOld .=@. rSloppy
       rSloppy    .=@. rSloppy - α*s
       rSloppyOld .=@. rSloppy - rSloppyOld
 
-	  # preconditioned residual
+      # preconditioned residual
       Precond(u, rSloppy)
-      #
-	  r2_old = r2
-	  # compute remaining dot products
-	  r2   = norm2(rSloppy)
+
+      r2_old = r2
+      # compute remaining dot products
+      r2   = norm2(rSloppy)
       γ    = rdot(u, rSloppy)
       γaux = rdot(u, rSloppyOld)
       γnew = γaux >= 0.0 ? γaux : γ
-			#
+
       β    = γnew / γold
-      #
+
+      rNormOld = rNorm
       rNorm   = sqrt(r2)
+
+      if(rNormOld < rNorm); aux_rel_trigger -= 1; end
 
       # xSloppy .= xSloppy + α*p <=> norm2(xSloppy) = norm2(xSloppy) + α*α*norm2(p)
       xnorm = xnorm + α*α*ppnorm
       dkm1  = dk
       #dk    = dkm1 + ϵ*rNorm+ϵh*abs(α)*Anorm*sqrt(xnorm)
-	  dk  = dkm1 + ϵ*sqrt(rNorm + (Anorm*sqrt(xnorm) + (nfact+4.0)*abs(α)*Anorm*sqrt(ppnorm)))
+      dk  = dkm1 + ϵ*sqrt(rNorm + (Anorm*sqrt(xnorm) + (nfact+4.0)*abs(α)*Anorm*sqrt(ppnorm)))
 
       updateR =  ( ((dkm1 <= deps*sqrt(r2_old)) && ((dk > (deps * rNorm)))) && (dk > dfac * dinit) )
 	  #updateR = ( (dkm1 <= (deps*sqrt(r2_old))) && ((dk > (deps * rNorm))) )
@@ -177,13 +177,13 @@ function solver(x::AbstractArray, b::AbstractArray, Mat::Any, MatSloppy::Any, pa
         y .=@. x + y
         Mat(r, y)
         r .=@. b - r
-		# Reset sloppy residual and solution vectors
+        # Reset sloppy residual and solution vectors
         cpy(rSloppy, r)
-		xSloppy .=@. 0.0
-		# preconditioned residual
-		Precond(u, rSloppy)
+        xSloppy .=@. 0.0
+        # preconditioned residual
+        Precond(u, rSloppy)
         #
-		r2 = norm2(r)
+        r2 = norm2(r)
 		#
         γ  = rdot(rSloppy, u)
         # Reorthogonalize previous search direction against the residual vector
@@ -195,7 +195,7 @@ function solver(x::AbstractArray, b::AbstractArray, Mat::Any, MatSloppy::Any, pa
         dinit = ϵh*(sqrt(r2) + Anorm*sqrt(norm2(y)))
         dk = dinit; xnorm = 0.0
 
-        if(sqrt(γ) > rNorm)
+        if sqrt(γ) > rNorm
           resIncrease      += 1
           resIncreaseTotal += 1
           println("Update residual is higher than iterative residual..", sqrt(γ), " the previous iter residual is ", rNorm)
@@ -210,7 +210,7 @@ function solver(x::AbstractArray, b::AbstractArray, Mat::Any, MatSloppy::Any, pa
       # Update iter index
       k += 1
 
-     @printf("%s: %d iteration, iter residual: %le  , true residual %le \n", solver_name, k, sqrt(γ) / sqrt(b2), sqrt(norm2(yaux)) / sqrt(b2))
+      @printf("%s: %d iteration, iter residual: %le  \n", solver_name, k, sqrt(γ) / sqrt(b2))
 
     end #while
 
