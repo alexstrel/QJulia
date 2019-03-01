edec152b73561f0cf8f272ce6b0c2da259a0e0f1
alexstrel <astrel@fnal.gov>
2019-02-28 22:49:27 -0600

    added improved fma methods

--- core/QJuliaIntrinsics.jl
+++ core/QJuliaIntrinsics.jl
@@ -13,6 +13,19 @@ m256    = QJuliaRegisters.m256
 m512d   = QJuliaRegisters.m512d
 m512    = QJuliaRegisters.m512
 
+m256dfma(a,b,c) = ccall("llvm.fma.v4f64", llvmcall, m256d, (m256d, m256d, m256d), a, b, c)
+m256fma(a,b,c ) = ccall("llvm.fma.v8f32", llvmcall, m256, (m256, m256, m256), a, b, c)
+
+m512dfma(a,b,c) = ccall("llvm.fma.v8f64", llvmcall, m512d, (m512d, m512d, m512d), a, b, c)
+m512fma(a,b,c ) = ccall("llvm.fma.v16f32", llvmcall, m512, (m512, m512, m512), a, b, c)
+
+prefetch(a, i1, i2, i3) = ccall("llvm.prefetch", llvmcall, Cvoid, (Ptr{Cchar}, Int32, Int32, Int32), a, i1, i2, i3)
+
+prefetchT0(a) = mmprefetch(a, Int32(0), Int32(3), Int(1))
+prefetchT1(a) = mmprefetch(a, Int32(0), Int32(2), Int(1))
+prefetchT2(a) = mmprefetch(a, Int32(0), Int32(1), Int(1))
+
+
 @inline function mm_mul(x::T, y::T) where T <:AbstractFloat
    return x * y
 end
@@ -133,23 +146,13 @@ end
 end
 
 @inline function mm_mad(a::T, xmm::m256, ymm::m256) where T <: AbstractFloat
-  (VecElement(a * xmm[1].value + ymm[1].value),
-   VecElement(a * xmm[2].value + ymm[2].value),
-   VecElement(a * xmm[3].value + ymm[3].value),
-   VecElement(a * xmm[4].value + ymm[4].value),
-   VecElement(a * xmm[5].value + ymm[5].value),
-   VecElement(a * xmm[6].value + ymm[6].value),
-   VecElement(a * xmm[7].value + ymm[7].value),
-   VecElement(a * xmm[8].value + ymm[8].value))
+   zmm = m256((a, a, a, a, a, a, a, a))
+   return m256fma(zmm,xmm,ymm)
 end
 
 @inline function mm_mad(a::T, xmm::m256d, ymm::m256d) where T <: AbstractFloat
-  (VecElement(a * xmm[1].value + ymm[1].value),
-   VecElement(a * xmm[2].value + ymm[2].value),
-   VecElement(a * xmm[3].value + ymm[3].value),
-   VecElement(a * xmm[4].value + ymm[4].value))
+   zmm = m256d((a, a, a, a))
+   return m256dfma(zmm,xmm,ymm)
 end
 
 end #QJuliaIntrinsics
-
-
